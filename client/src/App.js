e5baf0550f066a78d98491815cc2c6766f669e35
MatricksDeCoder <infinitumcubed@gmail.com>
2020-10-06 15:17:06 +0200

    shell create react app frontend

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './App.css'
+class App extends Component {
+  state = {
+    cow: '',
+    text: ''
+  }
+componentDidMount() {
+    this.fetchCow()
+  }
+fetchCow = async () => {
+    const response = await fetch(`/api/cow`)
+    const initialCow = await response.json()
+    const cow = initialCow.moo
+    this.setState({ cow })
+  }
+customCow = async evt => {
+    evt.preventDefault()
+    const text = this.state.text
+    const response = await fetch(`/api/cow/${text}`)
+    const custom = await response.json()
+    const cow = custom.moo
+    this.setState({ cow, text: '' })
+  }
+handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value })
+    console.log(this.state.text)
+  }
+render() {
+    return (
+      <div className="App">
+        <h3>Text Cow. Moo</h3>
+        <code>{this.state.cow}</code>
+        <form onSubmit={this.customCow}>
+          <label>Custom Cow Text:</label>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Show me a talking cow!</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default App
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
+const path = require('path')
 
 // Create the server
 const app = express()
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
   try {
@@ -25,13 +30,10 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
   }
 })
 
-app.get('/', cors(), async (req, res, next) => {
-    try {
-      res.json("Mmo is here!")
-    } catch (err) {
-      next(err)
-    }
-  })
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 
 // Choose the port and start the server
