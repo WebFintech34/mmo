a54dab4e2f1261d1ac1534246945fcabd4c0973c
MatricksDeCoder <infinitumcubed@gmail.com>
2020-10-06 13:38:38 +0200

    shell express backend

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cowsay = require('cowsay')
+const cors = require('cors')
+
+// Create the server
+const app = express()
+// Serve our api route /cow that returns a custom talking text cow
+app.get('/api/cow/:say', cors(), async (req, res, next) => {
+  try {
+    const text = req.params.say
+    const moo = cowsay.say({ text })
+    res.json({ moo })
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Serve our base route that returns a Hello World cow
+app.get('/api/cow/', cors(), async (req, res, next) => {
+  try {
+    const moo = cowsay.say({ text: 'Hello World!' })
+    res.json({ moo })
+  } catch (err) {
+    next(err)
+  }
+})
+
+app.get('/', cors(), async (req, res, next) => {
+    try {
+      res.json("Mmo is here!")
+    } catch (err) {
+      next(err)
+    }
+  })
+
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`)
+})
