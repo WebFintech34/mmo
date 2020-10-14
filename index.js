0c1d97d16b8e3f20909262e945af3b32a92197d2
WillzMu <wilfredmulenga@yahoo.com>
2020-10-15 01:19:49 +0200

    revert API to 5000

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function authenticatetoken ( req, res, next) {
 }
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Mixing it up on port ${PORT}`);
 });
