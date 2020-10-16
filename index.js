ec50431c8e268e2b65ba3cb128380f0ef72c9043
wac <40550299+WagnerCenteio@users.noreply.github.com>
2020-10-16 16:51:33 -0100

    Resolving conflicts

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -6,19 +5,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const router = require("./backend/routes");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 dotenv.config();
-=======
-const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
-const path = require('path')
->>>>>>> 19b077c2043d0c897119409324c385c5089fff0b
 
 console.log(process.env.MONGO_URL);
 
 // Create the server
-<<<<<<< HEAD
 const app = express();
 // database connection
 mongoose
@@ -40,24 +32,6 @@ router(app);
 
 // API docs
 app.use("/apidoc", express.static("public/docs"));
-=======
-const app = express()
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
->>>>>>> 19b077c2043d0c897119409324c385c5089fff0b
-
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
