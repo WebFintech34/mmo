32984c822970c5cb0334d7ff7255371ae48201ea
gloria <merogloria@gmail.com>
2020-10-22 18:19:05 +0300

    new

--- client/src/components/routes/Register.js
+++ client/src/components/routes/Register.js
@@ -11,10 +11,11 @@ export default function Register() {
             <h3>Registration page</h3>
             <div className="form">
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>Name:
                     <input
-                        name="name"
-                        class="form-control"
-                        placeholder="Name"
+                        name="First name"
+                        class="form-control name"
+                        placeholder="First name"
                         type="text"
                         ref={register({
                             required: "Required",
@@ -22,8 +23,21 @@ export default function Register() {
                     />
 
                     <input
+                        name="name"
+                        class="form-control Sname"
+                        placeholder="Second Name"
+                        type="text"
+                        ref={register({
+                            required: "Required",
+                        })}
+                    />
+                    </label>
+                   
+                 <fieldset>
+                     <legend>Personal Info:</legend>
+                     <input
                         name="gender"
-                        class="form-control"
+                        class="form-control gender"
                         placeholder="Gender"
                         type="text"
                         ref={register({
@@ -32,8 +46,38 @@ export default function Register() {
                     />
 
                     <input
+                        name="Birth Date"
+                        class="form-control date"
+                        placeholder="Birth Date"
+                        type="text"
+                        ref={register({
+                            required: "Required",
+                        })}
+                    />
+
+                    <input
+                        name="phone number"
+                        class="form-control phone"
+                        placeholder="Phone number"
+                        type="text"
+                        ref={register({
+                            required: "Required",
+                        })}
+                    />
+
+                    <input
+                        name="location"
+                        class="form-control place"
+                        placeholder="Location"
+                        type="text"
+                        ref={register({
+                            required: "Required",
+                        })}
+                    />
+
+                    <input
                         name="email"
-                        class="form-control"
+                        class="form-control email"
                         placeholder="Email"
                         type="text"
                         ref={register({
@@ -45,39 +89,31 @@ export default function Register() {
                         })}
                     />
                     {errors.email && errors.email.message}
-                    <input
-                        name="phone number"
-                        class="form-control"
-                        placeholder="Phone number"
-                        type="text"
+                    
+                     <input
+                        class="form-control pass"
+                        placeholder="Create password"
+                        type="password"
                         ref={register({
                             required: "Required",
                         })}
                     />
 
                     <input
-                        name="Birth Date"
-                        class="form-control"
-                        placeholder="Birth Date"
-                        type="text"
+                        class="form-control Rpass"
+                        placeholder="Repeat password"
+                        type="password"
                         ref={register({
                             required: "Required",
                         })}
                     />
-
-                    <input
-                        name="location"
-                        class="form-control"
-                        placeholder="Location"
-                        type="text"
-                        ref={register({
-                            required: "Required",
-                        })}
-                    />
+                 </fieldset>
+                 <fieldset>
+                     <legend className="leg">Questions:</legend>
 
                     <input
                         name="bool"
-                        class="form-control"
+                        class="form-control inp"
                         placeholder="NewBie? Yes or No"
                         type="text"
                         ref={register({
@@ -87,7 +123,7 @@ export default function Register() {
 
                     <input
                         name="amount"
-                        class="form-control"
+                        class="form-control amount"
                         placeholder="Amount willing to risk!"
                         type="text"
                         ref={register({
@@ -97,7 +133,7 @@ export default function Register() {
 
                     <input
                         name="bool"
-                        class="form-control"
+                        class="form-control bul"
                         placeholder="Are you aware of scams?  Yes or No"
                         type="text"
                         ref={register({
@@ -107,33 +143,15 @@ export default function Register() {
 
                     <input
                         name="bool"
-                        class="form-control"
+                        class="form-control bull"
                         placeholder="Have you been scammed before? Yes or No"
                         type="text"
                         ref={register({
                             required: "Required",
                         })}
                     />
-
-                    <input
-                        class="form-control"
-                        placeholder="Create password"
-                        type="password"
-                        ref={register({
-                            required: "Required",
-                        })}
-                    />
-
-                    <input
-                        class="form-control"
-                        placeholder="Repeat password"
-                        type="password"
-                        ref={register({
-                            required: "Required",
-                        })}
-                    />
-
-                    <div class="form-group">
+                    </fieldset>
+                    <div class="form-control">
                         <button type="submit" class="btn btn-primary btn-block">
                             Create Account
                         </button>
@@ -142,7 +160,7 @@ export default function Register() {
                         Have an account? <a href="/login">Log In</a>
                     </p>
                 </form>
-                <Link to="/"> back home</Link>
+                <Link className="new" to="/"> back home</Link>
             </div>
         </div>
     );
